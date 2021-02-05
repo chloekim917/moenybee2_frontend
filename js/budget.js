7036b20c5c6a43819f08e90114daa4ed9e0dd42c
chloekim917 <chloekim917@gmail.com>
2021-02-05 17:14:26 -0500

    budget initial

---
+++ js/budget.js
@@ -0,0 +1,22 @@
+document.addEventListener('submit', function(e){
+    let budgetForm = document.querySelector('.budget-form')
+    let budgetCategory = document.getElementById('categories')
+
+    if(e.target === budgetForm){
+
+        e.preventDefault()
+        fetch(`http://localhost:3000/api/v1/categories/${parseInt(budgetCategory.value)}`, {
+            method: "PATCH",
+            headers: {
+                'Content-Type': 'application/json',
+                'Accept': 'application/json'
+            },
+            body: JSON.stringify({
+                budget_amount: budgetForm.bamount.value
+            })
+        })
+        
+    }
+    budgetForm.reset()
+ 
+})
