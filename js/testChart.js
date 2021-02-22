a62eeda8e88d073e0b9ce87688935fe3167f722f
chloekim917 <chloekim917@gmail.com>
2021-02-22 18:00:44 -0500

    graph test4

--- js/testChart.js
+++ js/testChart.js
@@ -1,51 +1,51 @@
-var ctx = document.getElementById('chart').getContext('2d');
-var w = 1000, h = 800, maxR = 10, numItems = 500;
+// var ctx = document.getElementById('chart').getContext('2d');
+// var w = 1000, h = 800, maxR = 10, numItems = 500;
 
-var chart = new Chart(ctx, {
-  type: 'bubble',
-  // type: 'bar',
-  // type: 'doughnut',
-  // type: 'line',
-  options: {
-    legend: {
-      display: false
-    }
-  },
-  data: {
-    datasets: [
-      {
-        data: [],
-      }
-    ]
-  }
-});
+// var chart = new Chart(ctx, {
+//   type: 'bubble',
+//   // type: 'bar',
+//   // type: 'doughnut',
+//   // type: 'line',
+//   options: {
+//     legend: {
+//       display: false
+//     }
+//   },
+//   data: {
+//     datasets: [
+//       {
+//         data: [],
+//       }
+//     ]
+//   }
+// });
 
-function getData(numItems) {
-  let data = [];
+// function getData(numItems) {
+//   let data = [];
   
-  for(var i = 0; i < numItems; i++) {
-    data.push({
-      x: w * Math.random(),
-      y: h * Math.random(),
-      r: maxR * Math.random()
-    });
-  }
+//   for(var i = 0; i < numItems; i++) {
+//     data.push({
+//       x: w * Math.random(),
+//       y: h * Math.random(),
+//       r: maxR * Math.random()
+//     });
+//   }
   
-  return data;
-}
+//   return data;
+// }
 
-function updateChart(data) {
-  chart.data.datasets[0].data = data;
-  chart.update();
-}
+// function updateChart(data) {
+//   chart.data.datasets[0].data = data;
+//   chart.update();
+// }
 
-updateChart(getData(numItems));
+// updateChart(getData(numItems));
 
-document.getElementById('num-items-input').addEventListener('change', function(e) {
-  numItems = +e.target.value;
-  updateChart(getData(numItems));
-});
+// document.getElementById('num-items-input').addEventListener('change', function(e) {
+//   numItems = +e.target.value;
+//   updateChart(getData(numItems));
+// });
 
-document.getElementById('update-button').addEventListener('click', function() {
-  updateChart(getData(numItems));
-});
+// document.getElementById('update-button').addEventListener('click', function() {
+//   updateChart(getData(numItems));
+// });
