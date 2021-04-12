3a89cd6d433e5996a02d0b2cae85791b0a9e3ef5
chloekim917 <chloekim917@gmail.com>
2021-04-12 17:08:48 -0400

    deploy prepare3

--- js/bar.js
+++ js/bar.js
@@ -1,204 +1,204 @@
-//need to change graph by month
-document.addEventListener("DOMContentLoaded", function(e){
+// //need to change graph by month
+// document.addEventListener("DOMContentLoaded", function(e){
 
-    function addData(chart, data) {
-     //console.log(chart)
-    //console.log(data)
-    // console.log(chart.data.datasets[0].data)
+//     function addData(chart, data) {
+//      //console.log(chart)
+//     //console.log(data)
+//     // console.log(chart.data.datasets[0].data)
 
-    let newArr = []
+//     let newArr = []
 
-    newArr.push(expenseCategory.Food, expenseCategory.Utilities, expenseCategory.Entertainment, 
-        expenseCategory.Housing, expenseCategory.Transportation, expenseCategory.PersonalCare, 
-        expenseCategory.Gift, expenseCategory.Miscellaneous, expenseCategory.Travel, 
-        expenseCategory.Medical, expenseCategory.PetCare)
-    // console.log(newArr)
+//     newArr.push(expenseCategory.Food, expenseCategory.Utilities, expenseCategory.Entertainment, 
+//         expenseCategory.Housing, expenseCategory.Transportation, expenseCategory.PersonalCare, 
+//         expenseCategory.Gift, expenseCategory.Miscellaneous, expenseCategory.Travel, 
+//         expenseCategory.Medical, expenseCategory.PetCare)
+//     // console.log(newArr)
 
        
-    let chartArray =  chart.data.datasets[0].data
-    chartArray.splice(0, chartArray.length, ... newArr) 
-    //console.log(chartArray)
+//     let chartArray =  chart.data.datasets[0].data
+//     chartArray.splice(0, chartArray.length, ... newArr) 
+//     //console.log(chartArray)
 
-    chart.update();
-    }
+//     chart.update();
+//     }
 
    
-    function addBarData(chart, data) {
-        //console.log(chart)
-    //console.log(data)
-     //console.log(chart.data.datasets[1].data)
+//     function addBarData(chart, data) {
+//         //console.log(chart)
+//     //console.log(data)
+//      //console.log(chart.data.datasets[1].data)
 
-        let newArray = []
+//         let newArray = []
     
-        newArray.push(budgetExpenses.Food, budgetExpenses.Utilities, budgetExpenses.Entertainment, 
-            budgetExpenses.Housing, budgetExpenses.Transportation, budgetExpenses.PersonalCare, 
-            budgetExpenses.Gift, budgetExpenses.Miscellaneous, budgetExpenses.Travel, 
-            budgetExpenses.Medical, budgetExpenses.PetCare)
-        //console.log(newArray)
+//         newArray.push(budgetExpenses.Food, budgetExpenses.Utilities, budgetExpenses.Entertainment, 
+//             budgetExpenses.Housing, budgetExpenses.Transportation, budgetExpenses.PersonalCare, 
+//             budgetExpenses.Gift, budgetExpenses.Miscellaneous, budgetExpenses.Travel, 
+//             budgetExpenses.Medical, budgetExpenses.PetCare)
+//         //console.log(newArray)
     
            
-        let barChartArray = chart.data.datasets[1].data
-        barChartArray.splice(0, barChartArray.length, ... newArray) 
-        //console.log(barChartArray)
+//         let barChartArray = chart.data.datasets[1].data
+//         barChartArray.splice(0, barChartArray.length, ... newArray) 
+//         //console.log(barChartArray)
     
-        chart.update();
-        }
+//         chart.update();
+//         }
 
 
-let barData = {
-    labels: [
-        "Food","Utilities","Entertainment",
-        "Housing", "Transportation", "Personal Care", 
-        "Gift", "Miscellaneous", "Travel", "Groceries", 
-        "Medical"
-        ],
+// let barData = {
+//     labels: [
+//         "Food","Utilities","Entertainment",
+//         "Housing", "Transportation", "Personal Care", 
+//         "Gift", "Miscellaneous", "Travel", "Groceries", 
+//         "Medical"
+//         ],
 
-    datasets: [{
-        label:"Actual Spending",
-        data: [20, 30, 40, 10, 20, 30, 40, 10,20, 30, 40],
-        backgroundColor: ["#1E90FF", "#1E90FF", "#1E90FF","#1E90FF",
-        "#1E90FF","#1E90FF", "#1E90FF","#1E90FF", "#1E90FF", "#1E90FF",
-        "#1E90FF","#1E90FF"]
-    }, 
-    {   label: "Budgeted Amount",
-        data2:[11, 3, 14, 10, 21, 35, 47, 11, 26, 32, 41],
-        backgroundColor: ["#00CED1", "#00CED1", "#00CED1", "#00CED1", 
-        "#00CED1", "#00CED1", "#00CED1", "#00CED1",  "#00CED1",  "#00CED1", 
-        "#00CED1", "#00CED1"]
-    }]
-};
+//     datasets: [{
+//         label:"Actual Spending",
+//         data: [20, 30, 40, 10, 20, 30, 40, 10,20, 30, 40],
+//         backgroundColor: ["#1E90FF", "#1E90FF", "#1E90FF","#1E90FF",
+//         "#1E90FF","#1E90FF", "#1E90FF","#1E90FF", "#1E90FF", "#1E90FF",
+//         "#1E90FF","#1E90FF"]
+//     }, 
+//     {   label: "Budgeted Amount",
+//         data2:[11, 3, 14, 10, 21, 35, 47, 11, 26, 32, 41],
+//         backgroundColor: ["#00CED1", "#00CED1", "#00CED1", "#00CED1", 
+//         "#00CED1", "#00CED1", "#00CED1", "#00CED1",  "#00CED1",  "#00CED1", 
+//         "#00CED1", "#00CED1"]
+//     }]
+// };
 
-let barOptions = {
-    legend: {
-      position: 'left'
+// let barOptions = {
+//     legend: {
+//       position: 'left'
 
-    },
-    animation: {
-      animateRotate: false,
-      animateScale: true
-    }
-  };
+//     },
+//     animation: {
+//       animateRotate: false,
+//       animateScale: true
+//     }
+//   };
   
 
-    let barGraph = document.getElementById("bar-graph").getContext("2d");
+//     let barGraph = document.getElementById("bar-graph").getContext("2d");
     
-    const barChart2= 
-    new Chart(barGraph, {
-        type: 'bar',
-        data: barData,
-        options: barOptions
-    });
+//     const barChart2= 
+//     new Chart(barGraph, {
+//         type: 'bar',
+//         data: barData,
+//         options: barOptions
+//     });
 
 
-function fetchData(url){
-    fetch(url)
-    .then(resp => resp.json())
-    .then(expenditureData => {
-        expenditureData.data.forEach(expenditureObj => {
-            let categoryName = expenditureObj.attributes.category.name
-            let categoryAmount = expenditureObj.attributes.amount
-            let budgetAmount = expenditureObj.attributes.category.budget_amount
+// function fetchData(url){
+//     fetch(url)
+//     .then(resp => resp.json())
+//     .then(expenditureData => {
+//         expenditureData.data.forEach(expenditureObj => {
+//             let categoryName = expenditureObj.attributes.category.name
+//             let categoryAmount = expenditureObj.attributes.amount
+//             let budgetAmount = expenditureObj.attributes.category.budget_amount
             
             
-            if(categoryName === "Food"){
-                expenseCategory.Food += categoryAmount
-                budgetExpenses.Food += budgetAmount
-                //console.log(budgetExpenses.Food += budgetAmount)
-            }
-            else if(categoryName === "Utility"){
-                expenseCategory.Utilities += categoryAmount
-                budgetExpenses.Utilities += budgetAmount
-            }
-            else if(categoryName === "Entertainment"){
-                expenseCategory.Entertainment += categoryAmount
-                budgetExpenses.Entertainment += budgetAmount
-            }
-            else if(categoryName === "Housing"){
-                expenseCategory.Housing += categoryAmount
-                budgetExpenses.Housing += budgetAmount
-            }
+//             if(categoryName === "Food"){
+//                 expenseCategory.Food += categoryAmount
+//                 budgetExpenses.Food += budgetAmount
+//                 //console.log(budgetExpenses.Food += budgetAmount)
+//             }
+//             else if(categoryName === "Utility"){
+//                 expenseCategory.Utilities += categoryAmount
+//                 budgetExpenses.Utilities += budgetAmount
+//             }
+//             else if(categoryName === "Entertainment"){
+//                 expenseCategory.Entertainment += categoryAmount
+//                 budgetExpenses.Entertainment += budgetAmount
+//             }
+//             else if(categoryName === "Housing"){
+//                 expenseCategory.Housing += categoryAmount
+//                 budgetExpenses.Housing += budgetAmount
+//             }
            
-            else if(categoryName === "Transportation"){
-                expenseCategory.Transportation += categoryAmount
-                budgetExpenses.Transportation += budgetAmount
-            }
+//             else if(categoryName === "Transportation"){
+//                 expenseCategory.Transportation += categoryAmount
+//                 budgetExpenses.Transportation += budgetAmount
+//             }
            
-            else if(categoryName === "Personal Care"){
-                expenseCategory.PersonalCare += categoryAmount
-                budgetExpenses.PersonalCare += budgetAmount
-            }
+//             else if(categoryName === "Personal Care"){
+//                 expenseCategory.PersonalCare += categoryAmount
+//                 budgetExpenses.PersonalCare += budgetAmount
+//             }
            
-            else if(categoryName === "Gift"){
-                expenseCategory.Gift += categoryAmount
-                budgetExpenses.Gift += budgetAmount
-            }
+//             else if(categoryName === "Gift"){
+//                 expenseCategory.Gift += categoryAmount
+//                 budgetExpenses.Gift += budgetAmount
+//             }
            
-            else if(categoryName === "Miscellaneous"){
-                expenseCategory.Miscellaneous += categoryAmount
-                budgetExpenses.Miscellaneous += budgetAmount
-            }
+//             else if(categoryName === "Miscellaneous"){
+//                 expenseCategory.Miscellaneous += categoryAmount
+//                 budgetExpenses.Miscellaneous += budgetAmount
+//             }
 
-            else if(categoryName === "Travel"){
-                expenseCategory.Travel += categoryAmount
-                budgetExpenses.Travel += budgetAmount
-            }
+//             else if(categoryName === "Travel"){
+//                 expenseCategory.Travel += categoryAmount
+//                 budgetExpenses.Travel += budgetAmount
+//             }
             
-            else if(categoryName === "Groceries"){
-                expenseCategory.Groceries += categoryAmount
-                budgetExpenses.Groceries += budgetAmount
-            }
-            else if(categoryName === "Medical"){
-                expenseCategory.Medical += categoryAmount
-                budgetExpenses.Medical += budgetAmount
-            }
+//             else if(categoryName === "Groceries"){
+//                 expenseCategory.Groceries += categoryAmount
+//                 budgetExpenses.Groceries += budgetAmount
+//             }
+//             else if(categoryName === "Medical"){
+//                 expenseCategory.Medical += categoryAmount
+//                 budgetExpenses.Medical += budgetAmount
+//             }
 
-            })
+//             })
             
-            addData(barChart2,expenseCategory)
-            addBarData(barChart2,budgetExpenses)
-        })
+//             addData(barChart2,expenseCategory)
+//             addBarData(barChart2,budgetExpenses)
+//         })
     
-    }
+//     }
 
 
-fetchData("http://localhost:3000/api/v1/expenditures")
-  //iterate through array and grab category name DONE
-  //create counter for every category to get total
-  //total will replace value of matching key in expense Object
+// fetchData("http://localhost:3000/api/v1/expenditures")
+//   //iterate through array and grab category name DONE
+//   //create counter for every category to get total
+//   //total will replace value of matching key in expense Object
 
   
-  // console.log(expenseCategory)
+//   // console.log(expenseCategory)
 
 
-  let expenseCategory = {
-    Food:0,
-    Utilities: 0, 
-    Entertainment: 0, 
-    Housing: 0, 
-    Transportation:0, 
-    PersonalCare: 0, 
-    Gift:0, 
-    Miscellaneous: 0, 
-    Travel:0,
-    Groceries:0,
-    Medical:0, 
-    PetCare:0
-  }
+//   let expenseCategory = {
+//     Food:0,
+//     Utilities: 0, 
+//     Entertainment: 0, 
+//     Housing: 0, 
+//     Transportation:0, 
+//     PersonalCare: 0, 
+//     Gift:0, 
+//     Miscellaneous: 0, 
+//     Travel:0,
+//     Groceries:0,
+//     Medical:0, 
+//     PetCare:0
+//   }
 
-  let budgetExpenses = {
-    Food:0,
-    Utilities: 0, 
-    Entertainment: 0, 
-    Housing: 0, 
-    Transportation:0, 
-    PersonalCare: 0, 
-    Gift:0, 
-    Miscellaneous: 0, 
-    Travel:0,
-    Groceries:0,
-    Medical:0, 
-    PetCare:0
-  }
+//   let budgetExpenses = {
+//     Food:0,
+//     Utilities: 0, 
+//     Entertainment: 0, 
+//     Housing: 0, 
+//     Transportation:0, 
+//     PersonalCare: 0, 
+//     Gift:0, 
+//     Miscellaneous: 0, 
+//     Travel:0,
+//     Groceries:0,
+//     Medical:0, 
+//     PetCare:0
+//   }
 
 
-})
+// })
--- js/testChart.js
+++ js/testChart.js
@@ -1,196 +1,67 @@
-// var ctx = document.getElementById('chart').getContext('2d');
-// var w = 1000, h = 800, maxR = 10, numItems = 500;
+var ctx = document.getElementById('chart').getContext('2d');
+var w = 1000, h = 800, maxR = 10, numItems = 500;
 
-// var chart = new Chart(ctx, {
-//   type: 'bubble',
-//   // type: 'bar',
-//   // type: 'doughnut',
-//   // type: 'line',
-//   options: {
-//     legend: {
-//       display: false
-//     }
-//   },
-//   data: {
-//     datasets: [
-//       {
-//         data: [],
-//       }
-//     ]
-//   }
-// });
-
-// function getData(numItems) {
-//   let data = [];
-  
-//   for(var i = 0; i < numItems; i++) {
-//     data.push({
-//       x: w * Math.random(),
-//       y: h * Math.random(),
-//       r: maxR * Math.random()
-//     });
-//   }
-  
-//   return data;
-// }
-
-// function updateChart(data) {
-//   chart.data.datasets[0].data = data;
-//   chart.update();
-// }
-
-// updateChart(getData(numItems));
-
-// document.getElementById('num-items-input').addEventListener('change', function(e) {
-//   numItems = +e.target.value;
-//   updateChart(getData(numItems));
-// });
-
-// document.getElementById('update-button').addEventListener('click', function() {
-//   updateChart(getData(numItems));
-// });
-
-
-
-// // For a pie chart
-// var myPieChart = new Chart(ctx, {
-//   type: 'pie',
-//   data: data,
-//   options: options
-// });
-// // And for a doughnut chart
-// var myDoughnutChart = new Chart(ctx, {
-//   type: 'doughnut',
-//   data: data,
-//   options: options
-// });
-
-
-var randomScalingFactor = function() {
-  return Math.round(Math.random() * 100);
-};
-
-var config = {
-  type: 'doughnut',
-  data: {
-    datasets: [{
-      data: [
-        randomScalingFactor(),
-        randomScalingFactor(),
-        randomScalingFactor(),
-        randomScalingFactor(),
-        randomScalingFactor(),
-      ],
-      backgroundColor: [
-        window.chartColors.red,
-        window.chartColors.orange,
-        window.chartColors.yellow,
-        window.chartColors.green,
-        window.chartColors.blue,
-      ],
-      label: 'Dataset 1'
-    }],
-    labels: [
-      'Red',
-      'Orange',
-      'Yellow',
-      'Green',
-      'Blue'
-    ]
-  },
+var chart = new Chart(ctx, {
+  type: 'bubble',
+  // type: 'bar',
+  // type: 'doughnut',
+  // type: 'line',
   options: {
-    responsive: true,
-    plugins: {
-      legend: {
-        position: 'top',
-      },
-      title: {
-        display: true,
-        text: 'Chart.js Doughnut Chart'
-      },
-    },
-    animation: {
-      animateScale: true,
-      animateRotate: true
+    legend: {
+      display: false
     }
+  },
+  data: {
+    datasets: [
+      {
+        data: [],
+      }
+    ]
   }
-};
+});
 
-window.onload = function() {
-  var ctx = document.getElementById('chart-area').getContext('2d');
-  window.myDoughnut = new Chart(ctx, config);
-};
-
-document.getElementById('randomizeData').addEventListener('click', function() {
-  config.data.datasets.forEach(function(dataset) {
-    dataset.data = dataset.data.map(function() {
-      return randomScalingFactor();
+function getData(numItems) {
+  let data = [];
+  
+  for(var i = 0; i < numItems; i++) {
+    data.push({
+      x: w * Math.random(),
+      y: h * Math.random(),
+      r: maxR * Math.random()
     });
-  });
-
-  window.myDoughnut.update();
-});
-
-var colorNames = Object.keys(window.chartColors);
-document.getElementById('addDataset').addEventListener('click', function() {
-  var newDataset = {
-    backgroundColor: [],
-    data: [],
-    label: 'New dataset ' + config.data.datasets.length,
-  };
-
-  for (var index = 0; index < config.data.labels.length; ++index) {
-    newDataset.data.push(randomScalingFactor());
-
-    var colorName = colorNames[index % colorNames.length];
-    var newColor = window.chartColors[colorName];
-    newDataset.backgroundColor.push(newColor);
   }
+  
+  return data;
+}
 
-  config.data.datasets.push(newDataset);
-  window.myDoughnut.update();
+function updateChart(data) {
+  chart.data.datasets[0].data = data;
+  chart.update();
+}
+
+updateChart(getData(numItems));
+
+document.getElementById('num-items-input').addEventListener('change', function(e) {
+  numItems = +e.target.value;
+  updateChart(getData(numItems));
 });
 
-document.getElementById('addData').addEventListener('click', function() {
-  if (config.data.datasets.length > 0) {
-    config.data.labels.push('data #' + config.data.labels.length);
-
-    var colorName = colorNames[config.data.datasets[0].data.length % colorNames.length];
-    var newColor = window.chartColors[colorName];
-
-    config.data.datasets.forEach(function(dataset) {
-      dataset.data.push(randomScalingFactor());
-      dataset.backgroundColor.push(newColor);
-    });
-
-    window.myDoughnut.update();
-  }
+document.getElementById('update-button').addEventListener('click', function() {
+  updateChart(getData(numItems));
 });
 
-document.getElementById('removeDataset').addEventListener('click', function() {
-  config.data.datasets.splice(0, 1);
-  window.myDoughnut.update();
+
+
+// For a pie chart
+var myPieChart = new Chart(ctx, {
+  type: 'pie',
+  data: data,
+  options: options
+});
+// And for a doughnut chart
+var myDoughnutChart = new Chart(ctx, {
+  type: 'doughnut',
+  data: data,
+  options: options
 });
 
-document.getElementById('removeData').addEventListener('click', function() {
-  config.data.labels.splice(-1, 1); // remove the label first
-
-  config.data.datasets.forEach(function(dataset) {
-    dataset.data.pop();
-    dataset.backgroundColor.pop();
-  });
-
-  window.myDoughnut.update();
-});
-
-document.getElementById('changeCircleSize').addEventListener('click', function() {
-  if (window.myDoughnut.options.circumference === 180) {
-    window.myDoughnut.options.circumference = 360;
-    window.myDoughnut.options.rotation = -45;
-  } else {
-    window.myDoughnut.options.circumference = 180;
-    window.myDoughnut.options.rotation = -90;
-  }
-
-  window.myDoughnut.update();
-});
